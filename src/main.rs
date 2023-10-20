bdf866c361c2e8ee1f0d3f57dddbe8bb5fb803c2
FanelliMarco <marcovittorio789@gmail.com>
2023-10-20 22:12:14 +0200

    main.rs
    Primo test che compila correttamente

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::fs::File;
+use std::io::{BufReader};
+mod Elements;
+use crate::Elements::Element;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("../assets/txt/elements.txt").unwrap();
+    let reader = BufReader::new(file);
+    
+    let hydrogen = Element::new(
+        "Hydrogen".to_string(),
+        (10,12,14)
+    );
+
+    println!("{}", hydrogen);
+
+/*
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split("\\-").collect();
+
+        if parts.len() == 2 {
+            let rgb: Vec<&str> = parts[1].split(',').collect();
+            let r = rgb[0].parse::<u8>().unwrap();
+            let g = rgb[1].parse::<u8>().unwrap();
+            let b = rgb[2].parse::<u8>().unwrap();
+
+            let element = Element::Element {
+                name: parts[0].to_string(),
+                rgb: (r, g, b)
+            };
+
+            elements.push(element);
+        }
+    }
+    for element in &elements {
+        println!("{}", element.name);
+        println!(" - R:{}, G:{}, B:{}",
+                 element.rgb.0,
+                 element.rgb.1,
+                 element.rgb.2,
+        );
+    }
+    */
 }
+
+
